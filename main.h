ca8453a608a5d9cd9e0c3f171406a3592ed0caec
Andrew Trotman <andrew@cs.otago.ac.nz>
2023-08-21 17:18:50 +1200

    Added header file for Doxygen

---
+++ main.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/*!
+	@file
+*/
+
+/*!
+	@brief This method gets called
+*/
+void call_me(void);
+
+/*!
+	@brief This method does not get called
+*/
+void dead_code(void);
+
+/*!
+	@brief The program starts at this method
+*/
+int main(void);
+
